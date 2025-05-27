b0cef3e7e555edf1dee1cbb9a4d322f2ce0757dc
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-05-27 06:02:49 +0300

    scripts modularity

--- src/insert_data.py
+++ src/insert_data.py
@@ -1,28 +1,32 @@
-import os 
-
+from constants import DB_PARAMS, CONN_STR, PROJECT_PATH
 from sqlalchemy import create_engine
 import pandas as pd 
-from pathlib import Path
-
-DB_PARAMS = {
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT"),
-    "database": os.getenv("DB_DATABASE"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-}
-
-conn_str = f"postgresql://{DB_PARAMS['user']}:{DB_PARAMS['password']}@{DB_PARAMS['host']}:{DB_PARAMS['port']}/{DB_PARAMS['database']}"
-engine = create_engine(conn_str)
 
 
-with engine.connect() as conn:
+def check_table_exists(engine, table_name):
+    """
+    function checks if table exists in the database.
+    """
     try:
-        df = pd.read_sql_table(DB_PARAMS['database'], conn, chunksize=10)
-        print("df already exists")
+        with engine.connect as conn:
+            df = pd.read_sql_table(table_name, conn, chunksize=10)
+            print(df.head(2))
+        return True
     except ValueError:
-        print("table not found uploading it....")
-        
-        project_path = Path("/usr/app/")
-        df = pd.read_excel(project_path/"data/online_retail.xlsx")
-        df.to_sql("raw_online_retail", engine, if_exists="replace", index=False)
+        return False
+
+
+def load_raw_data(path, engine, table_name):
+    "function loads raw data to database"
+    df = pd.read_excel(path)
+    with engine.connect as conn:
+        df.to_sql(table_name, engine, if_exists="replace", index=False)
+
+
+engine = create_engine(CONN_STR)
+table_name = DB_PARAMS['database']
+data_path = PROJECT_PATH/"data/online_retail.xlsx"
+
+if __name__ == "__main__":
+    if not check_table_exists(engine, table_name):
+        load_raw_data(data_path, engine, "raw_online_retail")
