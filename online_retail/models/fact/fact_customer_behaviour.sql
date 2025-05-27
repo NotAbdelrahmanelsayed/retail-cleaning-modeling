50dd3740c95c5bf74ad4a1f10e917d53da5412cd
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-05-27 05:23:27 +0300

    recency/churn

--- online_retail/models/fact/fact_customer_behaviour.sql
+++ online_retail/models/fact/fact_customer_behaviour.sql
@@ -1,16 +1,34 @@
-{{ config(materialized='table')}}
+{{ config(materialized='table') }}
 
-WITH reference AS (
+WITH aggregated_metrics AS (
     SELECT 
-        MAX(invoice_year_month) AS max_date
-    
+        customer_id,
+        COUNT(*) AS total_orders,
+        SUM(total_invoice) AS total_spent,
+        ROUND(AVG(total_invoice), 3) AS average_order_value
+    FROM 
+        {{ ref('int_sales_analysis') }}
+    GROUP BY 
+        customer_id
 )
-SELECT 
-    customer_id,
-    COUNT(*) AS total_orders,
-    SUM(total_invoice) AS total_spent
-FROM 
-    {{ ref('int_sales_analysis') }}
-GROUP BY 
-    customer_id
 
+SELECT 
+    am.customer_id,
+    am.total_orders,
+    am.total_spent,
+    am.average_order_value,
+    DATE_PART('day', ld.snapshot_date - lp.last_order_date) AS resency_day,
+    CASE 
+        WHEN
+            ld.snapshot_date - lp.last_order_date > INTERVAL '90 days' THEN 1
+        ELSE 0
+    END AS churned
+
+FROM
+    aggregated_metrics am 
+LEFT JOIN 
+    {{ ref('date_customer_last_purchase') }} lp
+ON
+    am.customer_id = lp.customer_id
+CROSS JOIN 
+    {{ ref('date_last_date_reference') }} ld -- refer to models/dim/schema.yml to know about this table
