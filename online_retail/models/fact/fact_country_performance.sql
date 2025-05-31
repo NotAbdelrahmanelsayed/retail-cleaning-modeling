591b28685dab7cf3f98c6c83cb6682d3eabf41d9
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2025-05-31 10:51:13 +0300

    join dim customer

--- online_retail/models/fact/fact_country_performance.sql
+++ online_retail/models/fact/fact_country_performance.sql
@@ -1,11 +1,14 @@
 SELECT
-    country,
-    COUNT(DISTINCT customer_id) AS num_customers,
-    COUNT(order_id) AS num_orders,
-    SUM(total_invoice) AS total_revenue
+    dc.country,
+    COUNT(DISTINCT sa.customer_id) AS num_customers,
+    COUNT(sa.order_id) AS num_orders,
+    SUM(sa.total_invoice) AS total_revenue
 FROM
-    {{ ref('int_sales_analysis') }}
+    {{ ref('int_sales_analysis') }} sa
+LEFT JOIN 
+    {{ ref('dim_customer') }} dc
+USING (customer_id, order_id)
 WHERE 
-    country <> 'Unspecified'
+    dc.country <> 'Unspecified'
 GROUP BY
-    country
+    dc.country
